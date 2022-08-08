73860a5016b4864f73b434e11056d7198c74bddd
merleandrade <andrade.merle@gmail.com>
2022-08-08 16:41:20 +0200

    Add HelloController, SecurityConfig, userController

---
+++ src/main/java/com/example/testnutzername/TestnutzernameApplication.java
@@ -0,0 +1,13 @@
+package com.example.testnutzername;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestnutzernameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestnutzernameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testnutzername/TestnutzernameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testnutzername;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestnutzernameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
